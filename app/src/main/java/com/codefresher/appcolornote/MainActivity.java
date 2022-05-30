794fbb0b63ef2f0a67a5dae8cb732057df8feb83
ACER <aniviakid13@gmail.com>
2022-05-30 11:25:57 +0700

    Initial commit

---
+++ app/src/main/java/com/codefresher/appcolornote/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codefresher.appcolornote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
